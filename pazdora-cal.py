0ba252bde16d625652c69ce2adc668ea1cbd0188
wotsushi <wotsushi@gmail.com>
2018-10-31 23:05:56 +0900

    Improve the steps to count each drops in monte_carlo_freq

--- pazdora-cal.py
+++ pazdora-cal.py
@@ -36,12 +36,12 @@ def monte_carlo_freq(lack_cond, fields):
     num_ng = 0
     for drops in fields:
 
-        fire_drops_num = len([i for i in drops if i == 0])
-        blue_drops_num = len([i for i in drops if i == 1])
-        green_drops_num = len([i for i in drops if i == 2])
-        light_drops_num = len([i for i in drops if i == 3])
-        black_drops_num = len([i for i in drops if i == 4])
-        recovery_drops_num = len([i for i in drops if i == 5])
+        fire_drops_num = drops.count(0)
+        blue_drops_num = drops.count(1)
+        green_drops_num = drops.count(2)
+        light_drops_num = drops.count(3)
+        black_drops_num = drops.count(4)
+        recovery_drops_num = drops.count(5)
 
         if not lack_cond(
                 fire_drops_num,
